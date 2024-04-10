cdbdf9fa8fc5dbf28a2ae058007f10c7bbd486b4
Petitnuagedk <164025167+Petitnuagedk@users.noreply.github.com>
2024-04-10 15:11:48 +0200

    Update Ex1.py
    made it work

--- Ex1.py
+++ Ex1.py
@@ -60,8 +60,8 @@ def animateSimulation():
             plots[node] = plt.scatter(*coordinate, label=node)
 
         # Determine animation bounds
-        x_bounds = [0,0]
-        y_bounds = [0,0]
+        x_bounds = [9999,0]
+        y_bounds = [9999,0]
         for i in range(len(coordinatesHistoricCopy)):
             for (node, coordinate) in coordinatesHistoricCopy[i][1].items():
                 if (coordinate[0] > x_bounds[1]):
@@ -134,23 +134,63 @@ def scenario_test(nodes):
     mobility.SetMobilityModel ("ns3::WaypointMobilityModel")
     mobility.SetPositionAllocator(positions)
     mobility.Install(nodes)
-    
-    # Create a point-to-point helper
-    pointToPoint = ns.point_to_point.PointToPointHelper()
-    pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue("5Mbps"))
-    pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue("2ms"))
-    
+    # Create a point-to-point helper : only for N ----- N
+    # pointToPoint = ns.point_to_point.PointToPointHelper()
+    # pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue("5Mbps"))
+    # pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue("2ms"))
     # Install network devices and link them
-    devices = pointToPoint.Install(nodes)
-    
+    # devices = pointToPoint.Install(nodes) 
     # Add IP stack to nodes
+    
+    channel = ns.wifi.YansWifiChannelHelper.Default()
+    #Create a channel by using defrault Yet Another Network Simulator
+    #channel model, this channel servers for communication between wireless nodes
+
+    phy = ns.wifi.YansWifiPhyHelper()
+    #Create a helper object to configure physical layer of Wifi nodes
+    #Still use Yans
+    phy.SetChannel(channel.Create())
+    #Related to phy from previousline, create the channel with
+    #physical layer. Outter line is setting chaneel for wifi nodes
+    #By default, the WifiHelper (the typical use case for WifiPhy creation) will
+    #configure the WIFI_STANDARD_80211ax standard by default. 
+
+    #####Standard definiation undefined
+    # ns.wifi.SetStandard(WIFI_STANDAR4.Two clients with one server (UDP)----How two client communicate through the server???​D_80211n)
+    phy.Set("ChannelSettings", ns.core.StringValue("{0, 40, BAND_5GHZ, 0}"))
+    #the operating channel will be channel 38 in the 5 GHz band,
+    #which has a width of 40 MHz, and the primary20 channel will be the 20 MHz
+    #subchannel with the lowest center frequency (index 0).
+
+    mac = ns.wifi.WifiMacHelper()
+    #Create a helper object to configure MAC layer of wifi nodes
+    ssid = ns.wifi.Ssid("ns-3-ssid")
+    #Create a ssid for wifi network, which identifies a wifi network
+
+    wifi = ns.wifi.WifiHelper()
+    #Create a helper object to set up wifi network
+
+    mac.SetType(
+        "ns3::StaWifiMac", "Ssid", ns.wifi.SsidValue(ssid), "ActiveProbing", ns.core.BooleanValue(False)
+    )
+    #Set the type of MAC layer for station devices as 'ns3::StaWifiMac'
+    #Applied SSID for station device from precious definition
+    #Disable active probling from station device with ns.core.Boolean
+    staDevices = wifi.Install(phy, mac, nodes)
+    #Connect station device with station wifi nodes, applying precious
+    #physical and MAC layer settings, store them in staDevices
     stack = ns.internet.InternetStackHelper()
+    #Create an internetstack helper object to configure and install
+    #internet protocol stacks on nodes
     stack.Install(nodes)
     
+    stack = ns.internet.InternetStackHelper()
+    stack.Install(nodes)
+    ns.internet.Ipv4GlobalRoutingHelper.PopulateRoutingTables()
     # Assign IP addresses
     address = ns.internet.Ipv4AddressHelper()
     address.SetBase(ns.network.Ipv4Address("10.1.1.0"), ns.network.Ipv4Mask("255.255.255.0"))
-    interfaces = address.Assign(devices)
+    interfaces = address.Assign(staDevices)
     
     
     return nodes, interfaces
@@ -162,9 +202,9 @@ def coms_UDP(node_emit,node_reciev, interfaces, it):
     Parameters
     ----------
     node_emit : node object from ns library
-        The node use as the client
-    node_reciev : node object from ns library
         The node use as the server
+    node_reciev : index of the node object from ns library
+        The node use as the client
     interfaces : Address object from ns library
         Interfaces of the nodes (holds their addresses)
 
@@ -202,16 +242,15 @@ def animateWaypointWalkNodes(n_nodes):
     nodes, interfaces = setup(n_nodes,"test")
     print("nodes created, starting simulation...")
     
-    for node_i in range(n_nodes-2):
-        print("starting com setup...")
-
+    for node_i in range(n_nodes-1):
         it = 5001
         for node_j in range(node_i +1, n_nodes):
+            print("starting com setup between node ", node_i, " and node ", node_j)
             node_emit = nodes.Get(node_i).__deref__()
-            node_reci = nodes.Get(node_j).__deref__()
-            coms_UDP(node_emit, node_reci, interfaces,it)
-        it+=1
-        
+            coms_UDP(node_emit, node_j, interfaces,it)
+            it+=1
+    
+    print("com is setup")
     # We need to setup the waypoints each node will walk on
     # In this case, we are going to make them walk in
     # the DVD Logo fashion
@@ -266,6 +305,6 @@ def animateWaypointWalkNodes(n_nodes):
 
     animateSimulation()
 #%%
-n_nodes = 2
+n_nodes = 5
 animateWaypointWalkNodes(n_nodes)
 
